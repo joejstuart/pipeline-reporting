b40ddd9c817926b25e72d84f7eebfb12feaf95b6
jstuart <jstuart@redhat.com>
2018-06-28 12:30:34 -0500

    updating format

--- print_output.py
+++ print_output.py
@@ -7,7 +7,7 @@ import json
 import os
 
 
-def html_output(job_name, job_results):
+def html_output(job_name, job_results, measurement):
     page_content = list()
     page_content.append("<div style='width: 50%; border-bottom: 1px solid #A09B9A; padding: 5px;'>")
     page_content.append("<h1>{}</h1>".format(job_name))
@@ -21,8 +21,8 @@ def html_output(job_name, job_results):
     for result in job_results:
         page_content.append("<h5>{}</h5>".format(result))
         for build_results in job_results[result]:
+            dashboard_url = grafana_url.format(measurement, build_results[1])
             job_url = jenkins_url.format(job_name.replace('_', '-'), build_results[2])
-            page_content.append('<div>')
             influx_time = None
             for time_fmt in ('%Y-%m-%dT%H:%M:%S.%fZ', '%Y-%m-%dT%H:%M:%SZ'):
                 try:
@@ -37,9 +37,16 @@ def html_output(job_name, job_results):
                 continue
 
             fmt_time = influx_time.strftime('%m-%d-%y %H:%M:%S')
+            page_content.append('<div>')
+            page_content.append("<div style='padding-right: 5px; display:inline-block;'>")
             page_content.append("{}:".format(fmt_time))
-            page_content.append('&nbsp;')
+            page_content.append('</div>')
+            page_content.append("<div style='width: 100px; padding-right: 5px; display:inline-block;'>")
             page_content.append("<a href='{}'>{}</a>".format(job_url, build_results[1]))
+            page_content.append('</div>')
+            page_content.append("<div style='display:inline-block;'>")
+            page_content.append("<a href='{}'>Dashboard</a>".format(dashboard_url))
+            page_content.append('</div>')
             page_content.append('</div>')
 
     page_content.append('</div>')
@@ -59,7 +66,8 @@ pipeline_data = {
         'jenkins_url': "https://jenkins-continuous-infra.apps.ci.centos.org/view/Fedora%20All%20Packages%20Pipeline/job/{}/{}/console",
         'influxdb_url': 'influxdb-continuous-infra.apps.ci.centos.org',
         'build_prefix': 'Fedora_All_Packages_Pipeline',
-        'title': 'Fedora Pipeline'
+        'title': 'Fedora Pipeline',
+        'grafana_url': 'http://grafana-continuous-infra.apps.ci.centos.org/d/-esp4Gvmz/fedora-packages?orgId=1&amp;var-pipeline={}&amp;var-package={}'
     },
     'RHEL': {
         'jenkins_url': "",
@@ -74,12 +82,14 @@ jenkins_url = None
 influxdb = None
 build_prefix = None
 page_title = None
+grafana_url = None
 
 try:
     jenkins_url = pipeline_data[pipeline]['jenkins_url']
     influxdb_url = pipeline_data[pipeline]['influxdb_url']
     build_prefix = pipeline_data[pipeline]['build_prefix']
     page_title = pipeline_data[pipeline]['title']
+    grafana_url = pipeline_data[pipeline]['grafana_url']
 except KeyError:
     raise Exception('Invalid pipeline name')
 
@@ -91,8 +101,7 @@ for measurement in influxdb.measurements():
         job_name = influxdb.strip_prefix(measurement)
         if job_name:
             job_results = influxdb.all_job_data(measurement)
-            content.append(html_output(job_name, job_results))
-
+            content.append(html_output(job_name, job_results, measurement))
 
 confluence = metrics.Confluence(os.environ['CONFLUENCE_URL'])
 title = "%s: %s" % (page_title, datetime.date.today())
