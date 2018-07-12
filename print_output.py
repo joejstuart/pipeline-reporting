1aca9bd2790f0aee3c2cc330fa37e2648df95774
jstuart <jstuart@redhat.com>
2018-07-12 10:41:36 -0500

    updating grafana links

--- print_output.py
+++ print_output.py
@@ -9,8 +9,13 @@ import os
 
 def html_output(job_name, job_results, measurement):
     page_content = list()
+    today = datetime.datetime.today()
+    week_ago = today - datetime.timedelta(days=7)
+    today = int(today.timestamp()) * 1000
+    week_ago = int(week_ago.timestamp()) * 1000
+    all_pkgs_dashboard = all_pkgs_url.format(measurement, week_ago, today)
     page_content.append("<div style='width: 50%; border-bottom: 1px solid #A09B9A; padding: 5px;'>")
-    page_content.append("<h1>{}</h1>".format(job_name))
+    page_content.append("<h1><a style='color: #000000; text-decoration: none;' href='{}'>{}</a></h1>".format(all_pkgs_dashboard, job_name))
 
     if not len(job_results):
         page_content.append('<br />')
@@ -21,7 +26,7 @@ def html_output(job_name, job_results, measurement):
     for result in job_results:
         page_content.append("<h5>{}</h5>".format(result))
         for build_results in job_results[result]:
-            dashboard_url = grafana_url.format(measurement, build_results[1])
+            dashboard_url = grafana_url.format(week_ago, today, measurement, build_results[1])
             job_url = jenkins_url.format(job_name.replace('_', '-'), build_results[2])
             influx_time = None
             for time_fmt in ('%Y-%m-%dT%H:%M:%S.%fZ', '%Y-%m-%dT%H:%M:%SZ'):
@@ -41,7 +46,7 @@ def html_output(job_name, job_results, measurement):
             page_content.append("<div style='padding-right: 5px; display:inline-block;'>")
             page_content.append("{}:".format(fmt_time))
             page_content.append('</div>')
-            page_content.append("<div style='width: 100px; padding-right: 5px; display:inline-block;'>")
+            page_content.append("<div style='width: 150px; padding-right: 5px; display:inline-block;'>")
             page_content.append("<a href='{}'>{}</a>".format(job_url, build_results[1]))
             page_content.append('</div>')
             page_content.append("<div style='display:inline-block;'>")
@@ -67,7 +72,8 @@ pipeline_data = {
         'influxdb_url': 'influxdb-continuous-infra.apps.ci.centos.org',
         'build_prefix': 'Fedora_All_Packages_Pipeline',
         'title': 'Fedora Pipeline',
-        'grafana_url': 'http://grafana-continuous-infra.apps.ci.centos.org/d/-esp4Gvmz/fedora-packages?orgId=1&amp;var-pipeline={}&amp;var-package={}'
+        'grafana_all_pkgs': 'http://grafana-continuous-infra.apps.ci.centos.org/d/gJYUENmmk/upstream-fedora-pipeline?orgId=1&amp;var-measurement={}&amp;from={}&amp;to={}',
+        'grafana_url': 'http://grafana-continuous-infra.apps.ci.centos.org/d/-esp4Gvmz/fedora-packages?orgId=1&amp;from={}&amp;to={}&amp;var-pipeline={}&amp;var-package={}'
     },
     'RHEL': {
         'jenkins_url': "",
@@ -83,6 +89,7 @@ influxdb = None
 build_prefix = None
 page_title = None
 grafana_url = None
+all_pkgs_url = None
 
 try:
     jenkins_url = pipeline_data[pipeline]['jenkins_url']
@@ -90,6 +97,7 @@ try:
     build_prefix = pipeline_data[pipeline]['build_prefix']
     page_title = pipeline_data[pipeline]['title']
     grafana_url = pipeline_data[pipeline]['grafana_url']
+    all_pkgs_url = pipeline_data[pipeline]['grafana_all_pkgs']
 except KeyError:
     raise Exception('Invalid pipeline name')
 
